2624764a5923b97d5604fff38a9c6b385d7cc00e
R1013-T <nosuke1013@icloud.com>
2024-04-10 01:20:11 +0900

    refactor: add tech back

--- src/app/[lang]/_components/grid/computer/index.tsx
+++ src/app/[lang]/_components/grid/computer/index.tsx
@@ -6,7 +6,7 @@ import SettingSection from './setting'
 export default async function ComputerGrid({ lang }: { lang: string }) {
   return (
     <article className="h-full w-full p-4">
-      <div className="h-5/6 lg:h-[calc(100dvh-30px)] lg:overflow-y-hidden max-w-7xl mx-auto md:flex-wrap gap-2 lg:flex-nowrap hidden border-2 rounded-3xl p-3 md:flex">
+      <div className="h-5/6 lg:h-[calc(100dvh-24px)] max-w-7xl mx-auto md:flex-wrap gap-2 lg:flex-nowrap hidden border-2 rounded-3xl p-3 md:flex">
         <div className="w-full h-full flex flex-col gap-2">
           <ProfileSection lang={lang} />
           <MainSection lang={lang} />
