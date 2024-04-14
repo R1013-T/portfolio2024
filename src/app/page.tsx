72f3e10459f4ff04f10ceb751e10059799f901f6
R1013-T <nosuke1013@icloud.com>
2024-04-14 23:37:08 +0900

    feat: change top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 export default function Top() {
-  redirect('/ja')
+  // redirect('/ja')
+
+  return (
+    <main>
+      <Link href="/ja">ja</Link>
+      <Link href="/en">en</Link>
+    </main>
+  )
 }
