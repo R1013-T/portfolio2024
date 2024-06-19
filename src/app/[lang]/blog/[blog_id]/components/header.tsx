ed3c8fe031c4891157b39b1c067487cbc8ec5606
RYU <takahashi.ryunosuke.job@gmail.com>
2024-06-19 18:00:08 +0900

    feat: tags display

--- src/app/[lang]/blog/[blog_id]/components/header.tsx
+++ src/app/[lang]/blog/[blog_id]/components/header.tsx
@@ -8,11 +8,23 @@ import type { Article } from '@/types/article'
 import QiitaLogo from '../assets/qiita-icon.png'
 
 export default function BlogHeader({ article }: { article: Article }) {
+  const tags = article.tag_map.map((tag) => tag.tags.name)
+
   return (
     <section className="mt-20 mb-3 flex flex-col md:flex-row gap-3 justify-between">
       <div className="flex flex-col gap-2">
         <Text type="h1">{article.title}</Text>
         <Text type="p">{article.description}</Text>
+        <div className="flex gap-1">
+          {tags.map((tag, index) => (
+            <p
+              key={index}
+              className="border px-3 rounded py-2 text-xs text-foreground/70"
+            >
+              {tag}
+            </p>
+          ))}
+        </div>
         <p className="mt-1 text-sm text-foreground/70">{article.date}</p>
       </div>
       <div className="h-8 w-8">
