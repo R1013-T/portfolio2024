5cbe490f89d68e661fcb1aee3d25b2dee02c6d84
RYU <takahashi.ryunosuke.job@gmail.com>
2024-06-27 04:09:55 +0900

    feat: add loading message

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 
 export default function Loading() {
   return (
-    <section className="h-dvh w-full flex justify-center items-center">
+    <section className="h-dvh w-full flex flex-col gap-3 justify-center items-center">
       <Image src="/icon.webp" alt="Loading..." width={100} height={100} />
+      <p>当サイトをご覧いただきありがとうございます！</p>
     </section>
   )
 }
